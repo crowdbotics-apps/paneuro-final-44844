287939ae37236901fdd6759ff50158ad73b5f184
Crowdbotics <team@crowdbotics.com>
2023-12-01 21:15:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "paneuropeanholdings-44844",
+  url: "https://paneuropeanholdings-44844.botics.co",
+  api: "https://paneuropeanholdings-44844.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
